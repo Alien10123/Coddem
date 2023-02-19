6047e5666e5144208be31128f76de3fe03d634c6
Sam <samuelemanuelhart@icloud.com>
2023-02-19 19:50:10 +1100

    Text to Learn tab on the side of course viewer

--- src/routes/courses/[id]/+page.svelte
+++ src/routes/courses/[id]/+page.svelte
@@ -2,6 +2,9 @@
     <div class="w-screen bg-white h-8"></div>
     <div class="flex w-screen h-[calc(100vh-64px)]">
         <div class="w-14 h-full bg-slate-900">
+            <h1 class="font-sourceSans text-white text-2xl font-semibold ml-2 mt-2">Creating a tsconfig.json</h1>
+            <h3 class="text-xs text-gray-200 ml-2">Using Typescript in Node js</h3>
+            <p class="text-sm ml-2 text-white w-[45ch]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, alias? Non voluptas quisquam eius vitae minima nihil ipsa. Quisquam unde sed tenetur ex vel sint, deleniti recusandae doloremque nisi vitae.</p>
         </div>
         <div class="grow h-full flex flex-col">
             <div class="grow w-full min-h-[10%]"></div>
