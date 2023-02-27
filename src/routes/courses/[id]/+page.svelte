467a1055e2b9a30ea44f023f5f270a34184740a7
Sam <samuelemanuelhart@icloud.com>
2023-02-27 18:47:51 +1100

    Add codemirror

--- src/routes/courses/[id]/+page.svelte
+++ src/routes/courses/[id]/+page.svelte
@@ -1,8 +1,41 @@
-<div class="w-screen h-screen bg-slate-1000">
+<script lang="ts">
+	import { EditorView, basicSetup } from 'codemirror';
+	import { keymap } from '@codemirror/view';
+	import { indentWithTab } from '@codemirror/commands';
+	import { javascript } from '@codemirror/lang-javascript';
+	import { boysAndGirls } from 'thememirror';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	if (browser) {
+		onMount(() => {
+			if (document.querySelector('.cm-editor') === null) {
+				// let test = EditorView.baseTheme({
+				// 	'.cm-editor': {
+				// 		fontFamily: '"Fira Code", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
+				// 	}
+				// });
+				let view = new EditorView({
+					extensions: [
+						basicSetup,
+						javascript(),
+						boysAndGirls,
+						// @ts-ignore
+						keymap.of(indentWithTab)
+					]
+				});
+				console.log(view.dom);
+				document.getElementById('code')?.appendChild(view.dom);
+			}
+		});
+	}
+</script>
+
+<div class="w-screen h-screen bg-slate-1000 overflow-y-hidden">
 	<div class="w-screen bg-white h-8" />
 	<div class="flex w-screen h-[calc(100vh-64px)]">
-		<div class="w-14 h-full bg-slate-900 border-r-2 border-slate-500">
-			<div class="bg-slate-1000 h-7 w-full flex justify-center place-items-center gap-1">
+		<div class="w-14 h-full bg-slate-900 border-r-2 border-slate-500 overflow-scroll">
+			<div class="bg-slate-1000 h-7 w-full flex justify-center place-items-center gap-1 ">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="32"
@@ -18,27 +51,72 @@
 						d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"
 					/></svg
 				>
-                <h2 class="font-notoSans text-white font-bold">Tutorial</h2>
+				<h2 class="font-notoSans text-white font-bold">Tutorial</h2>
 			</div>
-            <h3 class="text-xs text-gray-200 ml-2 mt-3">Using Typescript in Node js</h3>
+			<h3 class="text-xs text-gray-200 ml-2 mt-3">Using Typescript in Node js</h3>
 			<h1 class="font-sourceSans text-white text-2xl font-semibold ml-2 leading-7">
 				Creating a tsconfig.json
 			</h1>
-            <div class="prose prose-invert">
-                <p class="text-sm ml-2 text-white w-[45ch] mt-3">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi sed ullam illo voluptatibus repudiandae minus cum quibusdam. Eligendi distinctio officia soluta, tenetur eum, eos nisi totam, nostrum perspiciatis excepturi asperiores?
-                    Exercitationem aperiam vitae aspernatur facere in nihil dolor! Eius doloribus fuga laboriosam, corporis, voluptate atque distinctio ex mollitia debitis sint quaerat eos voluptatem provident neque ab vitae praesentium rem tenetur?
-                    Ipsa commodi nisi, officiis amet exercitationem culpa tenetur. Sapiente assumenda magnam accusantium illum vitae eligendi, molestiae explicabo at quibusdam sint laborum! A aspernatur architecto soluta voluptatem consequatur. Laudantium, numquam quibusdam!
-                    Numquam dignissimos ducimus animi at doloremque quisquam quas, dolorum quidem illum. Distinctio dolore, fugiat voluptate esse velit ut suscipit facere doloribus nostrum, magni quam ea sequi, quis obcaecati voluptatem totam!
-                    Adipisci cupiditate provident hic molestiae, maiores nihil recusandae voluptas itaque? Ab nisi, tenetur eius nostrum fugiat facere dolorem sint architecto rerum. Quae consectetur provident non, enim modi sapiente consequuntur cum.
-                </p>
-            </div>
+			<div class="prose prose-invert">
+				<p class="text-sm ml-2 text-white w-[45ch] mt-3">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi sed ullam
+					illo voluptatibus repudiandae minus cum quibusdam. Eligendi distinctio officia
+					soluta, tenetur eum, eos nisi totam, nostrum perspiciatis excepturi asperiores?
+					Exercitationem aperiam vitae aspernatur facere in nihil dolor! Eius doloribus
+					fuga laboriosam, corporis, voluptate atque distinctio ex mollitia debitis sint
+					quaerat eos voluptatem provident neque ab vitae praesentium rem tenetur? Ipsa
+					commodi nisi, officiis amet exercitationem culpa tenetur. Sapiente assumenda
+					magnam accusantium illum vitae eligendi, molestiae explicabo at quibusdam sint
+					laborum! A aspernatur architecto soluta voluptatem consequatur. Laudantium,
+					numquam quibusdam! Numquam dignissimos ducimus animi at doloremque quisquam
+					quas, dolorum quidem illum. Distinctio dolore, fugiat voluptate esse velit ut
+					suscipit facere doloribus nostrum, magni quam ea sequi, quis obcaecati
+					voluptatem totam! Adipisci cupiditate provident hic molestiae, maiores nihil
+					recusandae voluptas itaque? Ab nisi, tenetur eius nostrum fugiat facere dolorem
+					sint architecto rerum. Quae consectetur provident non, enim modi sapiente
+					consequuntur cum.
+				</p>
+			</div>
 		</div>
-		<div class="grow h-full flex flex-col">
-			<div class="grow w-full min-h-[10%]" />
-			<div class="h-12 border-t-2 border-slate-500">
+		<div class="grow h-full flex flex-col" id="editor">
+			<div class="grow w-full min-h-[10%]" id="code" />
+			<div class="h-12 border-t-2 border-slate-500 min-h-[10%]">
 				<p class="text-md text-white font-code ml-2 mt-2">Hello World</p>
 			</div>
 		</div>
 	</div>
 </div>
+
+<svelte:head>
+	<style>
+		.cm-editor {
+			height: 100%;
+		}
+
+		.cm-focused {
+			outline: 0 !important;
+		}
+	</style>
+</svelte:head>
+
+<style>
+	#code :global(.cm-gutter) {
+		@apply font-code;
+	}
+
+	#code :global(.cm-content) {
+		@apply font-code;
+	}
+
+	#code :global(.cm-editor) {
+		@apply bg-slate-1000;
+	}
+
+	#code :global(.cm-editor), #code :global(.cm-gutters) {
+		@apply bg-slate-1000;
+	}
+
+	#code :global(.cm-activeLine), #code :global(.cm-activeLineGutter) {
+		@apply bg-gray-800;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: ['codemirror', '@codemirror/view', '@codemirror/commands', '@codemirror/lang-javascript', '@codemirror/theme-one-dark', 'thememirror']
+	}
 });
